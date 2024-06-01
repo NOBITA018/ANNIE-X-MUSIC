60d2bddf422898fc83984139da3f9ffcfe876a27
NOBITA🦋 <167997792+NOBITA018@users.noreply.github.com>
2024-06-01 12:51:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -103,7 +103,7 @@ def time_to_seconds(time):
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 if SUPPORT_CHANNEL:
-    if not re.match("https://t.me/+RN6PcuIKBHViMzll", SUPPORT_CHANNEL):
+    if not re.match("https://", SUPPORT_CHANNEL):
         raise SystemExit(
             "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
         )
